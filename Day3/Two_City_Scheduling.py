879c38ae0bd4ebe1a09a60671eaa4c36092b10d6
sohamsshah <47717492+sohamsshah@users.noreply.github.com>
2020-06-03 16:02:53 +0530

    Update Two_City_Scheduling.py

--- Day3/Two_City_Scheduling.py
+++ Day3/Two_City_Scheduling.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-
-
 @author: Soham Shah
 """
 
-costs = [[10,20],[30,200],[400,50],[30,20]]
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        sorted_costs = sorted(costs, key = lambda x: x[0] -x[1])
+        result = 0
+        for i in range(len(costs)):
+            if i< len(costs)/2:
+                result += sorted_costs[i][0]
+            else:
+                result += sorted_costs[i][1]
+        return result
+    
 
-sorted_costs = sorted(costs, key = lambda x: x[0] -x[1])
-
-result = 0
-for i in range(len(costs)):
-    if i< len(costs)/2:
-        result += sorted_costs[i][0]
-    else:
-        result += sorted_costs[i][1]
+        
     
         
